9da44c3f5f0272654fb79a8b21d8cadbe100c665
Vera Lyamzina <lyamzina91@gmail.com>
2023-07-28 21:33:26 +0300

    Правки до ДЗ

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import Axios from 'axios';
+import axios from 'axios';
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from 'notiflix';
@@ -8,6 +8,7 @@ import Notiflix from 'notiflix';
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadBtn = document.querySelector(`.load-more`);
+let lightbox;
 
 form.addEventListener('submit', onSearchForm);
 loadBtn.addEventListener('click', loadItems);
@@ -16,24 +17,20 @@ let page = 1;
 let name = ``;
 loadBtn.style.display = `none`;
 
-function onSearchForm(eve) {
-    cleanPage();
-    eve.preventDefault();
-    name = eve.currentTarget.elements.searchQuery.value.trim();
 
-    if (!name) {
-        Notiflix.Notify.warning(`Please choose the animal`);
-    } else {
-        fetchUrl(name, page);
+function updateGallery() {
+    if (lightbox) {
+        lightbox.refresh();
     }
 }
 
-async function fetchUrl(searchRequest, page = 1) {
+
+async function fetchData(searchRequest, page = 1) {
     try {
         const API_KEY = '38482679-fa06f355fbbaefa388f4c969f';
         const URL = `https://pixabay.com/api/`;
 
-        arrOfItems = await Axios.get(`${URL}`, {
+        const response = await axios.get(`${URL}`, {
             params: {
                 key: `${API_KEY}`,
                 image_type: 'photo',
@@ -45,30 +42,11 @@ async function fetchUrl(searchRequest, page = 1) {
 
             },
         });
-        console.log(arrOfItems);
 
-        if (arrOfItems.data.totalHits > 0 && page === 1) {
-            Notiflix.Notify.info(
-                `Hooray! We found ${arrOfItems.data.totalHits} images.`
-            );
-        }
-
-        if (arrOfItems.data.totalHits === 0) {
-            Notiflix.Notify.failure(
-                `Sorry, there are no images matching your search query. Please try again.`
-            );
-        }
-
-        renderMarkUp(arrOfItems.data);
-
-        new SimpleLightbox(`.gallery .photo-card`, {
-            captionType: 'attr',
-            captionsData: `alt`,
-            captionDelay: 250,
-        }).refresh();
+        return response.data;
 
     } catch (error) {
-        Notiflix.Notify.warning(error);
+        throw new Error(error);
     }
 }
 
@@ -90,23 +68,78 @@ function renderMarkUp(arr) {
         `);
     }, ``);
 
-    gallery.innerHTML = '';
+    // gallery.innerHTML = '';
     gallery.innerHTML = markUp;
 
     if (arr.hits.length > 0) {
         loadBtn.style.display = ``;
     }
 
-    if ((Math.floor(arr.totalHits / 40) < page) && arr.hits.length != 0) {
+    else
+    // ((Math.floor(arr.totalHits / 40) < page) && arr.hits.length != 0) 
+    {
         loadBtn.style.display = 'none';
         Notiflix.Notify.info(
             "We're sorry, but you've reached the end of search results."
         );
     }
+
+
+    updateGallery();
 }
 
-function loadItems() {
-    fetchUrl(name, (page += 1));
+
+
+async function onSearchForm(eve) {
+    cleanPage();
+    eve.preventDefault();
+    name = eve.currentTarget.elements.searchQuery.value.trim();
+
+    if (!name) {
+        Notiflix.Notify.warning(`Please choose the animal`);
+    } else {
+        try {
+            const data = await fetchData(name, page);
+            if (data.totalHits > 0 && page === 1) {
+                Notiflix.Notify.info(`Hooray! We found ${data.totalHits} images.`);
+            }
+
+            if (data.totalHits === 0) {
+                Notiflix.Notify.failure(
+                    `Sorry, there are no images matching your search query. Please try again.`
+                );
+            }
+            renderMarkUp(data);
+        } catch (error) {
+            Notiflix.Notify.warning(error.message);
+        }
+    }
+}
+
+function initLightbox() {
+    lightbox = new SimpleLightbox('.gallery .photo-card', {
+        captionType: 'attr',
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+}
+
+initLightbox();
+
+async function loadItems() {
+    try {
+        const data = await fetchData(name, page + 1);
+
+        if (data.hits.length > 0) {
+            page++;
+            renderMarkUp(data);
+        } else {
+            loadBtn.style.display = 'none';
+            Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
+        }
+    } catch (error) {
+        Notiflix.Notify.warning(error.message);
+    }
 }
 
 function cleanPage() {
